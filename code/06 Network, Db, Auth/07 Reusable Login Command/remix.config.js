e23117d9d08196d98d3bd756af3c7f7b493f195c
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2023-04-22 08:53:37 +0200

    updated remix config

--- code/06 Network, Db, Auth/07 Reusable Login Command/remix.config.js
+++ code/06 Network, Db, Auth/07 Reusable Login Command/remix.config.js
@@ -1,6 +1,7 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
+  serverPlatform: "node",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
