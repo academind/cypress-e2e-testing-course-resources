ce48722960ff05a9006bd7274336fac40c9884e7
Maximilian Schwarzmüller <28806196+maxschwarzmueller@users.noreply.github.com>
2023-04-22 10:38:10 +0200

    more remix config adjustments

--- code/06 Network, Db, Auth/04 Testing APIs/remix.config.js
+++ code/06 Network, Db, Auth/04 Testing APIs/remix.config.js
@@ -2,8 +2,11 @@
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
   serverPlatform: "node",
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  future: {
+    v2_errorBoundary: false,
+    v2_meta: false,
+    v2_normalizeFormMethod: false,
+    v2_routeConvention: false,
+    unstable_tailwind: false,
+  },
 };
